670b1f35be0d07286e4c119f405ba942c998e0ac
Mateusz Wojciechowski <mateuszwojciechowski@MacBook-Air-Mateusz.local>
2017-12-02 19:19:05 +0100

    added pulse interval regulation

---
+++ peripheraldisplaylibrary/src/main/java/com/mateuszwojciechowski/peripheraldisplaylibrary/DisplayCommand.java
@@ -0,0 +1,20 @@
+package com.mateuszwojciechowski.peripheraldisplaylibrary;
+
+/**
+ * Klasa abstrakcyjna deklarująca funkcje potrzebne do wysłania komendy
+ * @author Mateusz Wojciechowski
+ */
+
+abstract class DisplayCommand {
+    /**
+     * Metoda zwracająca treść komendy
+     * @return komenda wysyłana do wyświetlacza
+     */
+    public abstract String getCommand();
+
+    /**
+     * Metoda zwracająca czas trwania operacji
+     * @return czas trwania operacji
+     */
+    public abstract long getTime();
+}
